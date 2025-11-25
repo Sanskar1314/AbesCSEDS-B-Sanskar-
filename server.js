13d69fb8ffd1ddc418ef7d139402890caeaa7aac
Sanskar1314 <sanskargupta1314@gmail.com>
2025-11-25 11:31:15 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,7 +7,35 @@ app.get('/', (req, res)=>{
     res.send('this is my first express server');
 })
 app.get('/about', (req, res)=>{
-    res.send('this is my first about page');
+    // res.send('this is my first about page');
+    const students = [
+        {
+            id: 1,
+            name: 'Sanskar',
+            age: '20'
+        },
+        {
+            id: 2,
+            name: 'Ankit',
+            age: '20'
+        },
+        {
+            id: 3,
+            name: 'Udbhav',
+            age: '20'
+        },
+        {
+            id: 4,
+            name: 'Santosh',
+            age: '20'
+        },
+        {
+            id: 5,
+            name: 'Vaibhav',
+            age: '20'
+        }
+    ]
+    
 })
 app.get('/contact', (req, res)=>{
     res.send('<h1>7985674322 is my contact number</h1>');
